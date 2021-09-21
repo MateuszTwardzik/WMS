6f87b1bd5a312dbd41f51a697e536a0313c9c976
i3ero <i3ero@Outlook.com>
2021-09-21 16:34:53 +0200

    init

---
+++ MagazynApp/ViewModel/AlleyViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MagazynApp.ViewModel
+{
+    public class AlleyViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int SectorId { get; set; }
+    }
+}
